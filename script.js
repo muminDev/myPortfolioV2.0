135661d3201dff20c00f7ed3f7f11d0158a7853d
Muminjon <juramirzaev.muminjon@gmail.com>
2024-01-11 22:49:17 +0100

    contact section  modified and connected to google sheets

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ const sr = ScrollReveal({
 });
 sr.reveal(".home-text", { delay: 150, origin: "left" });
 sr.reveal(".home-img", { delay: 150, origin: "right" });
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxLu0fhI-TdBS9pvtB3pHOlf664lbwZnMhGtOkWlWPazAyEJ0EtHxd7C-6ayPbQv70W/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Email sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 4000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
